1fb68c0e44f7da3efd48854b84cfaf78aec00edb
expal <expal123@mail.ru>
2022-05-02 19:08:57 -0700

    hw3
    Сделал 6 заданий, json файл внутри

---
+++ mainapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+import json
+from datetime import datetime
+
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context_data = {}
+        with open('mainapp/1.json') as file:
+            context_data['object_list'] = json.load(file)
+        return context_data
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
+    # Create your views here.
